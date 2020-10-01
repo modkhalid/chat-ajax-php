be9156a950de992cb0b9c343567325b521d77f49
Mohammad Anas <anas.1633.m@gmail.com>
2020-10-01 22:15:23 +0530

    Update chat.php

--- chat.php
+++ chat.php
@@ -7,6 +7,7 @@
 				   <div class="row w3-border-bottom">
 				   	<div class="col-4">
 				   		<b style="color: green;"><?php echo $row['name'];?></b>
+						<h4>Hacktoberfest</h4>
 				   	</div>
 				   	<div class="col-6">
 				   		<p style="color: red;"><?php echo $row['msg'];?></p>
